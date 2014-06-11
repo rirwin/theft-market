4345be2c27bebf6979f0a722aaf54c12436ef083
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-06-10 22:04:33 -0700

    Added getting function calls

--- common/TruliaConfLoader.py
+++ common/TruliaConfLoader.py
@@ -4,14 +4,20 @@ class TruliaConfLoader:
     def __init__(self, config_path): 
         config = ConfigParser.ConfigParser()
         config.read(config_path + "/theft-market.conf")
-        self._url = config.get("trulia", "url")
-        self._api_key = config.get("trulia", "api-key")
-        self._location_library = config.get("trulia","location-library")
-        self._stats_library = config.get("trulia","stats-library")
+        self.url = config.get("trulia", "url")
+        self.api_key = config.get("trulia", "api-key")
+        self.location_library = config.get("trulia","location-library")
+        self.stats_library = config.get("trulia","stats-library")
 
+        loc_func_raw = config.get("trulia","location-functions")
+        self.location_functions=list(filter(None, (x.strip() for x in loc_func_raw.splitlines())))
+
+        stat_func_raw = config.get("trulia","stats-functions")
+        self.stats_functions=list(filter(None, (x.strip() for x in stat_func_raw.splitlines())))
 
 # unit-test
 if __name__ == "__main__":
     import pprint
-    trulia_conf_loader = TruliaConfLoader('../conf/')
-    pprint.pprint(vars(trulia_conf_loader))
+    tcl = TruliaConfLoader('../conf/')
+    pprint.pprint(vars(tcl))
+
