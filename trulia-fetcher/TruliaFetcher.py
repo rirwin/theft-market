5b4e31a228c59b857020ebe828fac1ec953ac73e
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-06-12 12:04:35 -0700

    Learned how to parse two-level xml. Definitely JSON is faster.

--- trulia-fetcher/TruliaFetcher.py
+++ trulia-fetcher/TruliaFetcher.py
@@ -17,13 +17,18 @@ class TruliaFetcher:
     def fetch_states(self):
         tc = self.trulia_conf
         url_str = tc.url + "library=" + tc.location_library + "&function=getStates&apikey=" + tc.apikey 
-        
+ 
+        '''       
         resp = urllib2.urlopen(url_str)
 
         if resp.code == 200:
             dest_dir = tc.data_dir + "/states_location_library"
             file_name = "getStates.xml"
-            self.save_xml_file(resp.read(), dest_dir, file_name)
+            text = resp.read()
+            self.save_xml_file(text, dest_dir, file_name)
+        '''
+        fh = open(tc.data_dir + "/states_location_library/getStates.xml", 'r')
+        self.parse_two_tags(fh.read(), "state", "name")
 
 
     def save_xml_file(self, text, dest_dir, file_name):
@@ -33,10 +38,30 @@ class TruliaFetcher:
         except OSError:
             pass # already exists, ignore
             
-        with open(dest_dir + "/" + file_name, 'wb') as stream:
+        with open(dest_dir + "/" + file_name, 'w') as stream:
             stream.write(str(text))
 
 
+    def parse_one_tag(self, text, tag):
+        dom = minidom.parseString(text)
+        tagged_items = dom.getElementsByTagName(tag)
+
+        tagged_list = []
+        for tagged_item in tagged_items:
+            print tagged_item.firstChild.nodeValue
+
+
+    def parse_two_tags(self, text, tag, subtag):
+        head_dom = minidom.parseString(text)
+
+        dom_list = head_dom.getElementsByTagName(tag)
+
+        for dom_i in dom_list:
+            node_list = dom_i.getElementsByTagName(subtag)
+            for node in node_list:
+                print node.firstChild.nodeValue
+
+            
 
 # unit-test
 if __name__ == "__main__":
