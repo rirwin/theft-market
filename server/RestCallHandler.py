96a4b6b478b3735b3ee70ab893bdcbacd327a516
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-06-25 23:16:18 +0000

    Added date checking

--- server/RestCallHandler.py
+++ server/RestCallHandler.py
@@ -1,5 +1,5 @@
 import json
-
+import datetime
 ### Functions for handling queries
 
 def handle_data_city_volume_query(hbase_mgr, params):
@@ -8,15 +8,32 @@ def handle_data_city_volume_query(hbase_mgr, params):
         params_dict = eval(params)
     except:
         return "400 - malformed parameters dictionary", 400
-
-    valid, msg = check_params_dict_keys(params_dict, ['state_code','city','num_bedrooms','start_date','end_date'])
     
+    valid, msg = check_params_dict_keys(params_dict, ['state_code','city','num_bedrooms','start_date','end_date'])
+
     if valid is False:
         return msg, 400
 
+    if is_valid_date(params_dict['start_date']) is False:
+        return "400 - malformed start_date parameter (YYYY-MM-DD needed)"
+
+    if is_valid_date(params_dict['end_date']) is False:
+        return "400 - malformed end_date parameter (YYYY-MM-DD needed)"
+    
+    
     
 
     return params + " are your parameters"
+
+
+def is_valid_date(date):
+
+    try:
+        datetime.datetime.strptime(date, '%Y-%m-%d')
+    except:
+            return False
+
+    return True
 
 
 def check_params_dict_keys(params_dict, args):
