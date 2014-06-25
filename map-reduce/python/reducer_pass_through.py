a7255c192b147c781df1905fb03763c6432cae35
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-06-25 06:59:27 +0000

    city MapReduce cleaning job. It took a couple minutes

--- map-reduce/python/reducer_pass_through.py
+++ map-reduce/python/reducer_pass_through.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python                                                                                             
-
 from operator import itemgetter
 import sys
 
+state_dict = {}
+
 for line in sys.stdin:
+
     # remove leading and trailing whitespace
     line = line.strip()
-    print line
-    '''kv  = line.split('\t')
-
-    print kv[0] + "\t" + kv[1] 
-    '''
+    
+    # for Hive queries
+    print line 
