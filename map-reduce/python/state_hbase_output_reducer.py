44818b530b0b8c260345f9fdff895cfdbc09fa6e
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-06-26 04:11:46 +0000

    more M/R in python

---
+++ map-reduce/python/state_hbase_output_reducer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from operator import itemgetter
+import sys
+
+
+# input comes from STDIN
+for line in sys.stdin:
+    # remove leading and trailing whitespace
+    line = line.strip()
+    
+
+print dict
+# do not forget to output the last word if needed!
+if current_word == word:
+    print '%s\t%s' % (current_word, current_count)
---
+++ map-reduce/python/state_json_reducer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python                                                                                             
+
+from operator import itemgetter
+import sys
+
+# Key is (state_code, num_bed) 
+
+state_dict = {}
+
+for line in sys.stdin:
+
+    # remove leading and trailing whitespace
+    line = line.strip()
+    
+    # for Hbase queries
+    words = line.split('\t')
+    
+    try:
+        state_code = words[0]
+        num_bed = words[1] # used as str below
+        week = words[2] 
+        avg_price = int(words[3])
+        num_list = int(words[4])
+
+        key = state_code + '-' + num_bed
+
+        # create key if not in state_dict
+        if key not in state_dict:
+            state_dict[key] = {}
+
+        # assumes column family 'cf'
+        state_dict[key]['cf:'+week] = {'n':num_list, 'a':avg_price}
+    except:
+        pass
+
+    
+import happybase
+#conn = happybase.Connection('172.31.11.76',9090)
+conn = happybase.Connection('localhost')
+table = conn.table('state_stats')
+for key in state_dict: # state bed key
+    for wk_key in state_dict[key]:
+        val = str(state_dict[key][wk_key])
+        table.put(key,{wk_key:val})
+
+
