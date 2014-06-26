3bb0eff09b2c6e1784fb76dcd3f8bd58e9385a82
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-06-26 04:08:19 +0000

    some attempted use of happybase in hadoop streaming

--- map-reduce/python/state_reducer.py
+++ map-reduce/python/state_reducer.py
@@ -13,8 +13,9 @@ for line in sys.stdin:
     line = line.strip()
     
     # for Hive queries
-    #print line 
+    print line
 
+    '''
     # for Hbase queries
     words = line.split('\t')
     
@@ -24,14 +25,26 @@ for line in sys.stdin:
         week = words[2] 
         avg_price = int(words[3])
         num_list = int(words[4])
+
+        key = state_code + '-' + num_bed
+
+        # create key if not in state_dict
+        if key not in state_dict:
+            state_dict[key] = {}
+
+        # assumes column family 'cf'
+        state_dict[key]['cf:'+week] = {'n':num_list, 'a':avg_price}
     except:
-        print line
+        pass
 
+    
+import happybase
+conn = happybase.Connection('172.31.11.76',9090)
+#conn = happybase.Connection('localhost')
+table = conn.table('state_stats')
+for key in state_dict: # state bed key
+    for wk_key in state_dict[key]:
+        val = str(state_dict[key][wk_key])
+        table.put(key,{wk_key:val})
 
-    key = state_code + '-' + num_bed
-    if key not in state_dict:
-        state_dict[key] = {}
-    state_dict[key][week] = {'n':num_list, 'a':avg_price}
-
-
-print state_dict['AZ-3']
+'''
