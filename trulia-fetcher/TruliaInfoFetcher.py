30cf98d9aca2e9aaafceb6cb66d2a53e746ef913
Ryan Irwin <ryan.irwin.cpe@gmail.com>
2014-07-19 23:25:27 -0700

    Made api calls threaded so I can call multiple at once. Reduced startup time by another 1/4 or so.  It used to take 6 minutes, now it takes 30 seconds.

--- trulia-fetcher/TruliaInfoFetcher.py
+++ trulia-fetcher/TruliaInfoFetcher.py
@@ -1,5 +1,6 @@
 import urllib2
 import time
+import threading
 import sys
 import os
 from xml.dom import minidom
@@ -17,6 +18,10 @@ class TruliaInfoFetcher:
         self.load_trulia_params(trulia_conf)
         self.db_mgr = DatabaseManager.DatabaseManager(config_path)
         self.curr_key_idx = 0
+        
+        # lock for threads to use to add to val_strs
+        self.lock = threading.Lock()
+        self.val_strs = list()
 
     def load_trulia_params(self, trulia_conf):
         self.stats_library = trulia_conf.stats_library
@@ -105,15 +110,173 @@ class TruliaInfoFetcher:
             print " city info retrieved"
             time.sleep(2.0/len(self.apikeys)) # trulia api restriction
 
-    def fetch_all_zipcodes(self):
+
+
+    def fetch_all_zipcodes_threaded(self):
+
         res = self.db_mgr.simple_select_query(self.db_mgr.conn, "info_state", "state_code")
+
+        state_codes = list(res)
+        state_code_idx = 0;
+        t1 = time.time()
+        
+        while state_code_idx < len(state_codes):
+           
+            threads = list()
+            self.val_strs = list()
+            num_threads = min(len(self.apikeys), len(state_codes)-state_code_idx)
+            
+            for thread_idx in xrange(num_threads):
+                state_code = state_codes[state_code_idx][0]
+                url_str = self.url + "library=" + self.location_library + "&function=getZipCodesInState&state=" + state_code + "&apikey=" + self.apikeys[thread_idx]
+                t = threading.Thread(target=self.fetch_zipcodes_in_state_threaded, args=[url_str, state_code])
+                t.start()
+                threads.append(t)
+                state_code_idx+=1
+
+            for t in threads:
+                t.join()
+
+            for val_str in self.val_strs:
+                self.db_mgr.simple_insert_query(self.db_mgr.conn, "info_zipcode", val_str)  
+
+            # make sure we don't use the same API within 2 seconds
+            t2 = time.time()
+            if t2 - t1 < 2.0:
+                time.sleep(2.0 - (t2 - t1))
+            t1 = t2
+
+    def fetch_zipcodes_in_state_threaded(self, url_str, state_code):
+        
+        print 'fetch_zipcodes threaded', state_code
+
+        resp = urllib2.urlopen(url_str)
+        if resp.code == 200:
+            dest_dir = self.data_dir + "/zipcodes_location_library"
+            file_name = "getZipCodesInState_state_EQ_" + state_code + "_.xml"
+            text = resp.read()
+
+            info_zipcodes_val_str = TruliaInfoFetcher.parse_get_zipcodes_in_state_resp(text, state_code)
+            self.lock.acquire()
+            self.val_strs.append(info_zipcodes_val_str)
+            self.lock.release()
+            print "zipcode info retrieved", state_code
+
+
+    def fetch_all_counties_threaded(self):
+
+        res = self.db_mgr.simple_select_query(self.db_mgr.conn, "info_state", "state_code")
+
+        state_codes = list(res)
+        state_code_idx = 0;
+        t1 = time.time()
+        
+        while state_code_idx < len(state_codes):
+           
+            threads = list()
+            self.val_strs = list()
+            num_threads = min(len(self.apikeys), len(state_codes)-state_code_idx)
+            
+            for thread_idx in xrange(num_threads):
+                state_code = state_codes[state_code_idx][0]
+                url_str = self.url + "library=" + self.location_library + "&function=getCountiesInState&state=" + state_code + "&apikey=" + self.apikeys[thread_idx]
+                t = threading.Thread(target=self.fetch_counties_in_state_threaded, args=[url_str, state_code])
+                t.start()
+                threads.append(t)
+                state_code_idx+=1
+
+            for t in threads:
+                t.join()
+
+            for val_str in self.val_strs:
+                self.db_mgr.simple_insert_query(self.db_mgr.conn, "info_county", val_str)  
+
+            # make sure we don't use the same API within 2 seconds
+            t2 = time.time()
+            if t2 - t1 < 2.0:
+                time.sleep(2.0 - (t2 - t1))
+            t1 = t2
+
+    def fetch_counties_in_state_threaded(self, url_str, state_code):
+        
+        print 'fetch_counties threaded', state_code
+
+        resp = urllib2.urlopen(url_str)
+        if resp.code == 200:
+            dest_dir = self.data_dir + "/counties_location_library"
+            file_name = "getCountiesInState_state_EQ_" + state_code + "_.xml"
+            text = resp.read()
+
+            info_counties_val_str = TruliaInfoFetcher.parse_get_counties_in_state_resp(text, state_code)
+            self.lock.acquire()
+            self.val_strs.append(info_counties_val_str)
+            self.lock.release()
+            print "county info retrieved", state_code
+
+
+    def fetch_all_cities_threaded(self):
+
+        res = self.db_mgr.simple_select_query(self.db_mgr.conn, "info_state", "state_code")
+
+        state_codes = list(res)
+        state_code_idx = 0;
+        t1 = time.time()
+        
+        while state_code_idx < len(state_codes):
+           
+            threads = list()
+            self.val_strs = list()
+            num_threads = min(len(self.apikeys), len(state_codes)-state_code_idx)
+            
+            for thread_idx in xrange(num_threads):
+                state_code = state_codes[state_code_idx][0]
+                url_str = self.url + "library=" + self.location_library + "&function=getCitiesInState&state=" + state_code + "&apikey=" + self.apikeys[thread_idx]
+                t = threading.Thread(target=self.fetch_cities_in_state_threaded, args=[url_str, state_code])
+                t.start()
+                threads.append(t)
+                state_code_idx+=1
+
+            for t in threads:
+                t.join()
+
+            for val_str in self.val_strs:
+                self.db_mgr.simple_insert_query(self.db_mgr.conn, "info_city", val_str)  
+
+            # make sure we don't use the same API within 2 seconds
+            t2 = time.time()
+            if t2 - t1 < 2.0:
+                time.sleep(2.0 - (t2 - t1))
+            t1 = t2
+
+    def fetch_cities_in_state_threaded(self, url_str, state_code):
+        
+        print 'fetch_cities threaded', state_code
+
+        resp = urllib2.urlopen(url_str)
+
+        if resp.code == 200:
+            dest_dir = self.data_dir + "/cities_location_library"
+            file_name = "getCitiesInState_state_EQ_" + state_code + "_.xml"
+            text = resp.read()
+
+            info_cities_val_str = TruliaInfoFetcher.parse_get_cities_in_state_resp(text, state_code)
+            self.lock.acquire()
+            self.val_strs.append(info_cities_val_str)
+            self.lock.release()
+            print "city info retrieved", state_code
+
+
+    def fetch_all_zipcodes(self):
+        res = self.db_mgrs[0].simple_select_query(self.db_mgr.conn, "info_state", "state_code")
         for state_code_tuple in list(res):
             self.fetch_zipcodes_in_state(state_code_tuple[0])
 
 
     def fetch_zipcodes_in_state(self, state_code):
         print "Fetching zipcodes in " + state_code
+
         res = self.db_mgr.simple_select_query(self.db_mgr.conn, "info_zipcode", "count(*)", "state_code='" + state_code + "'")
+
         if res[0][0] > 0:
             print " already retrieved"
             return # already called
@@ -130,7 +293,8 @@ class TruliaInfoFetcher:
             self.db_mgr.simple_insert_query(self.db_mgr.conn, "info_zipcode", info_zipcodes_val_str)
             print " zipcode info retrieved"
             time.sleep(2.0/len(self.apikeys)) # trulia api restriction
-
+        
+        
     def save_xml_file(self, text, dest_dir, file_name):
 
         try:
@@ -228,7 +392,6 @@ class TruliaInfoFetcher:
                 val_str+="(" + zipcode + ",\"" + state_code + "\"," + latitude + "," + longitude + "),"
 
 
-
         return val_str[:-1]
             
 
@@ -236,11 +399,11 @@ if __name__ == "__main__":
     import pprint
     tf = TruliaInfoFetcher('../conf/')
 
-    # must run database_manager.DatabaseManager (main) to reset DB tables
+    # This clears the theft market meta store
+    DatabaseManager.main()
     
     tf.fetch_all_states()
-    tf.fetch_all_counties()
-    tf.fetch_all_cities()
-    tf.fetch_all_zipcodes()
-    
-
+    tf.fetch_all_counties_threaded()
+    tf.fetch_all_zipcodes_threaded()
+    tf.fetch_all_cities_threaded()
+        
