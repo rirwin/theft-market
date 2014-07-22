07e58f671e194f15aff021dd32d91987d27c9d41
Ubuntu <ubuntu@ip-172-31-25-81.us-west-1.compute.internal>
2014-07-22 03:00:51 +0000

    Working fetcherwork in isolation

--- trulia-fetcher/FetcherWorker.py
+++ trulia-fetcher/FetcherWorker.py
@@ -12,23 +12,36 @@ class FetcherWorker(threading.Thread):
 
     def run(self):
         while 1:
+
             url_tup = self.url_queue.get()
             if url_tup is None:
                 # add end-of-queue markers for parsers
                 self.text_queue.put(None) 
                 # ends thread
                 break
+
             url = url_tup[0] + self.apikey 
             location = url_tup[1]
-
+            t1 = time.time()
+            
             # try twice for now, put in while loop next
-            resp = urllib2.urlopen(url_str)
+            print "fetching try 1", location
+            resp = urllib2.urlopen(url)
             if resp.code == 200: 
                 text = resp.read()
-                self.text_queue.put((text, location))
+                #self.text_queue.put((text, location))
             else:
+                print 'failed once', location
                 time.sleep(10)
-                resp = urllib2.urlopen(url_str)
+                print "fetching try 2", location
+                resp = urllib2.urlopen(url)
                 if resp.code == 200:
                     text = resp.read()
-                    self.text_queue.put((text, location))
+                    #self.text_queue.put((text, location))
+
+            print "done fetching", location
+
+            # make sure we don't use the same API key within 2 seconds
+            t2 = time.time()
+            if t2 - t1 < 2.0:
+                time.sleep(2.0 - (t2 - t1))
